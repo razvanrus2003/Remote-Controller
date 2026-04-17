3f801e68ecc6320084595e491bda8079ee62381b
Razvan Rus <razvanrus@gmail.com>
2026-04-18 00:22:12 +0300

    Add basic remote control
    Problem: Disconnect from current spike
    
    Signed-off-by: Razvan Rus <razvanrus@gmail.com>

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 package_name = 'remote_control'
 
 setup(
     name=package_name,
     version='0.1.0',
-    packages=[package_name],
-    install_requires=['setuptools'],
+    packages=find_packages(exclude=['test']),
+    install_requires=['setuptools', 'pygame'],
     zip_safe=True,
     maintainer='Robot Developer',
     maintainer_email='you@example.com',
     description='ROS2 package for remote controller laptop',
     license='Apache-2.0',
     tests_require=['pytest'],
+    data_files=[
+        ('share/ament_index/resource_index/packages', [f'resource/{package_name}']),
+        (f'share/{package_name}', ['package.xml']),
+    ],
     entry_points={
         'console_scripts': [
-            'keyboard_publisher = remote_control.keyboard_publisher:main',
-            'video_subscriber = remote_control.video_subscriber:main',
+            'keyboard_publisher = remote_control.keyboard_publisher:main'
         ],
     },
 )
